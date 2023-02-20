ad903abad5e1135f3800b190a0ad3d34897f1583
YousefSayes <68135583+YousefSayes@users.noreply.github.com>
2023-02-20 13:41:58 -0500

    gitignore, implemented allocation  & debug printing functions

--- c_garbo/frosting.c
+++ c_garbo/frosting.c
@@ -12,16 +12,44 @@ typedef struct Frode{
     char friegh; // frozen neighbors -> freigh, max 8
 } Frode;
 
+// prints given Frode
+void prode(Frode** plate, int i, int j){
+    Frode temp = plate[i][j];
+    printf("Frode (%i,%i)\nHumid:\t%i\nState:\t%i\nFreis:\t%i\n", i, j, temp.humidity, temp.state, temp.friegh); 
+}
+
+// Allocates square plate of length len with an attribute humidity 
+Frode** allocplate(int len, char hum){
+    Frode **t;
+
+    t = (Frode **) malloc(sizeof(Frode *) * len);
+
+    for (int i = 0; i < len; i++) {
+        t[i] = (Frode *) malloc(sizeof(Frode) * len);
+        for (int j = 0; j < len; j++) {
+            t[i][j].humidity = hum;
+            t[i][j].state = WET;
+            t[i][j].friegh = 0;
+        }
+    }
+
+    return t;    
+}
+
 // To be implemented
-int indep_freeze(int tempp);
-int flu_freeze(Frode* node);
+int indep_freeze(int tempp); // independent freezing of nodes
+int flu_freeze(Frode* node); // influenced freezing of nodes
 
 
 int main(int argc, char**argv){
     // inputs are temp and humidity
-    int temp = atoi(argv[1]);
-    int humidity = atoi(argv[2]);
+    char temp = atoi(argv[1]);
+    char humidity = atoi(argv[2]);
+    int side_len = atoi(argv[3]);
 
+    Frode** plate = allocplate(side_len, humidity);
+
+    prode(plate, 10, 15);
 
 }
 
