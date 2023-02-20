69da287ee488ad8fa1c4e0ba9718df49eb32e268
YousefSayes <68135583+YousefSayes@users.noreply.github.com>
2023-02-20 14:17:47 -0500

    implemented influenced freezing need to fix chance functions

--- c_garbo/frosting.c
+++ c_garbo/frosting.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 // States of nodes
 #define DRY 0
 #define WET 1
@@ -17,7 +18,7 @@ typedef struct Frode{
 // prints given Frode
 void prode(Frode** plate, int i, int j){
     Frode temp = plate[i][j];
-    printf("Frode (%i,%i)\nHumid:\t%i\nState:\t%i\nFreis:\t%i\n", i, j, temp.humidity, temp.state, temp.friegh); 
+    printf("Frode (%i,%i)\nHumid:\t%i\nState:\t%i\nFreis:\t%i\n\n", i, j, temp.humidity, temp.state, temp.friegh); 
 }
 
 // Allocates square plate of length len with an attribute humidity 
@@ -38,8 +39,6 @@ Frode** allocplate(int len, char hum){
     return t;    
 }
 
-// To be implemented
-
 // independent freezing of nodes
 int indep_freeze(int temp){
     time_t t;
@@ -47,12 +46,27 @@ int indep_freeze(int temp){
 
     double p = 11.6379+temp*0.362419;
     double prob = 1/(1+exp(p));
-    float chance = (float)rand()/(float)(RAND_MAX);
+
+    double chance = (double)rand()/(double)(RAND_MAX);
+
     return (chance < prob);
 }
 
-// influenced freezing of nodes
-int flu_freeze(Frode* node); 
+// influenced freezing of nodes, runs on WET nodes
+int flu_freeze(Frode node){
+    time_t t;
+    srand((unsigned) time(&t));
+
+    // prob is reduced if there are multiple freighs (multiple ice bridges being made)
+    double prob = ((double)(node.humidity))/(255.0 * node.friegh);
+    printf("Prob:\t%f\n", prob);
+
+    // this shit fucked, gotta fix (biased)
+    double chance = ((double)rand())/((double)(RAND_MAX));
+    printf("Chance:\t%f\n", chance);
+
+    return (chance < prob);
+} 
 
 
 int main(int argc, char**argv){
@@ -65,5 +79,8 @@ int main(int argc, char**argv){
 
     prode(plate, 10, 15);
 
+    Frode* p = &plate[22][25];
+    p->friegh = 1;
+    printf("Succ:\t%i\n", flu_freeze(*p));
 }
 
