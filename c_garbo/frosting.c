c9bf3ccc224baf897ec962290b03a3b8f76fb2f7
YousefSayes <68135583+YousefSayes@users.noreply.github.com>
2023-02-22 14:26:57 -0500

    freezing loop added

--- c_garbo/frosting.c
+++ c_garbo/frosting.c
@@ -12,6 +12,9 @@
 #define WET 1
 #define FROZEN 2
 
+// Available Humidity in Wet Pillars
+static int AHUM = 0;
+
 // Frosting Node -> Frode
 typedef struct Frode{
     char humidity; // 255 is highest hummidty possible, 0 is lowest relative humidity
@@ -26,10 +29,11 @@ void prode(Frode** plate, int i, int j){
 }
 
 // Produces random double from [0,max)
+// Taken from https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/rand-s?view=msvc-170
 double genprob(double max){
     unsigned int number;
     errno_t err;
-    err = rand_s( &number );
+    err = rand_s(&number);
     if (err != 0) {
         printf("The rand_s function failed!\n");
     }
@@ -70,15 +74,58 @@ int flu_freeze(Frode node){
 
     // prob is reduced if there are multiple freighs (multiple ice bridges being made)
     double prob = ((double)(node.humidity))/(255.0 * node.friegh);
-    printf("Prob:\t%f\n", prob);
+    // printf("Prob:\t%f\n", prob);
 
 
     double chance = genprob(1.0);
-    printf("Chance:\t%f\n", chance);
+    // printf("Chance:\t%f\n", chance);
 
     return (chance < prob);
 } 
 
+void addFreigh(Frode** plate, int i, int j, int len){
+    for (int k  = i-1; k < i+2; k++){
+        if (k<0 || k>=len) continue;
+
+        for (int m = j-1; m < j+2; m++){
+            if (m<0 || m>=len) continue;
+
+            if(!(k==i && m==j)) plate[i][j].friegh++;
+        }
+    }
+    
+}
+
+void freezing(Frode** plate, char temp, char humidity, int len){
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j < len; j++){
+            Frode cpy = plate[i][j];
+            if(cpy.state == WET){
+                // remove humidity boolean
+                int remHum = 0;
+
+                if(indep_freeze(temp)){
+                    plate[i][j].state = FROZEN;
+                    addFreigh(plate, i, j, len);
+                }
+
+                if(cpy.friegh){
+                    if(flu_freeze(cpy)){
+                        plate[i][j].state = FROZEN;
+                        addFreigh(plate, i, j, len);
+                    }else{
+                        plate[i][j].state = DRY;
+                    }
+                }
+
+                if(remHum){
+                    AHUM -= plate[i][j].humidity;
+                    plate[i][j].humidity = 0;
+                }
+            }
+        }
+    }
+}
 
 int main(int argc, char**argv){
     // inputs are temp and humidity
@@ -86,7 +133,10 @@ int main(int argc, char**argv){
     char humidity = atoi(argv[2]);
     int side_len = atoi(argv[3]);
 
+    AHUM = side_len*side_len*humidity;
+
     Frode** plate = allocplate(side_len, humidity);
+
 
     prode(plate, 10, 15);
 
